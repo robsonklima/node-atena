8a354f69ac4b4778b0c755120e163925919dc96d
robsonklima <robsonklima@gmail.com>
2018-07-16 18:02:31 -0300

    Update Project and Requirement

--- routes/requirements.js
+++ routes/requirements.js
@@ -4,10 +4,19 @@ const auth = require('../middleware/auth');
 const validate = require('../middleware/validate');
 const { Project } = require('../models/project');
 const { Requirement, validateRequirement } = require('../models/requirement');
+const validateObjectId = require('../middleware/validObjectId')
+
+router.get('/:id', async (req, res) => {
+  const project = await Project.findOne({ 'requirements._id': req.params.id });
+
+  if (!project) return res.status(404).send('The requirement with the given ID was not found.');
+
+  res.send(project.requirements.filter(r => { return r._id == req.params.id }));
+});
 
 router.post('/:id', [auth, validate(validateRequirement)], async (req, res) => {
   const project = await Project.findById(req.params.id);
-  if (!project) return res.status(404).send('The project with the given ID was not found.');
+  if (!project) return res.status(404).send('The requirement with the given ID was not found.');
 
   const requirement = new Requirement ({ name: req.body.name });
 
@@ -17,9 +26,30 @@ router.post('/:id', [auth, validate(validateRequirement)], async (req, res) => {
   res.send(requirement);
 });
 
-router.delete('/:projectId/:requirementId', [auth], async (req, res) => {
-  const project = await Project.findById(req.params.projectId);
-  if (!project) return res.status(404).send('The project with the given ID was not found.');
+router.put('/:id', validateObjectId, async (req, res) => {
+  const project = await Project.findOne({ 'requirements._id': req.params.id }).select('-__v');
+
+  if (!project) return res.status(404).send('The requirement with the given ID was not found.');
+
+  await Project.findOneAndUpdate(
+    { 
+      "requirements._id": req.params.id 
+    },
+    { 
+      "$set": { "requirements.$": req.body },
+    }, 
+    {
+      new: true 
+    },
+    function(err, result) {
+      res.send(result);
+    }
+  );
+});
+
+router.delete('/:id', [auth], async (req, res) => {
+  const project = await Project.findOne({ 'requirements._id': req.params.id }).select('-__v');
+  if (!project) return res.status(404).send('The requirement with the given ID was not found.');
 
   project.requirements.remove(req.params.requirementId);
   await project.save();
