c8ec7ac3a26b1d56a59243eb80ef1c18d68665ed
robsonklima <robsonklima@gmail.com>
2018-07-17 10:36:26 -0300

    REF

--- models/project.js
+++ models/project.js
@@ -6,7 +6,7 @@ const projectSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 1,
     maxlength: 50
   },
   requirements: [
@@ -18,7 +18,7 @@ const Project = mongoose.model('Project', projectSchema);
 
 function validateProject(project) {
   const schema = {
-    name: Joi.string().min(5).max(50).required()
+    name: Joi.string().min(1).max(50).required()
   };
 
   return Joi.validate(project, schema);
